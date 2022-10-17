eef8bead0c0c15a509ed752ed9ba5fc12afcdf23
harsha9724 <harshahr9724@gmail.com>
2022-10-17 12:33:57 +0530

    modified

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DATABASE_URL).then(() => {
     console.log("Connected to Database");
@@ -20,9 +20,9 @@ mongoose.connect(process.env.DATABASE_URL).then(() => {
 app.use("/", UserRouter);
 app.use("/", ContactsRouter)
 
-// app.get("*", (req, res) => {
-//     res.status(404).send("404 PAGE NOT FOUND")
-// })
+app.get("*", (req, res) => {
+    res.status(404).send("404 PAGE NOT FOUND")
+})
 
 app.listen(port, () => {
     console.log(`server is running at ${port}`);
